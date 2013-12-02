33922ac9684fe2a951bf1e765e1c105a8669aac8
Molo <arnaud@onesime.fr>
2013-12-02 10:44:53 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RestMobile::Application.config.secret_key_base = '453b10fb274f00f48c58f5989e715fb2d9cc90a3b83a9f81bf0f4315f95313fe8c37c18879ba2e06bfc0acba3113efb6770a3ce5f1f4ae6b6a42f5b7466e6bac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RestMobile::Application.config.session_store :cookie_store, key: '_rest-mobile_session'
